e2f7a7600b9159d25ed20e9d204812c738e9cd1e
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2018-10-02 19:54:21 +0530

    Read instructions

--- SortAnArray.cpp
+++ SortAnArray.cpp
@@ -1,37 +1,41 @@
 //This program sorts the numbers (0,1,2) in the given array
 #include<iostream>
-#include<stdlib.h>
 using namespace std;
-int main()
-{
+int main() {
 	int t,n;
 	int* arr;
 	cin>>t;
-	while(t--){
-		cin>>n;
-		arr=(int *)malloc(n*sizeof(int));
-		for(int i=0;i<n;i++){
-			cin>>arr[i];
+	while(t--) {
+		cin >> n;
+		arr = (int *)malloc(n*sizeof(int));
+		for( int i = 0;i < n; i++) {
+			cin >> arr[i];
 		}
-		for(int i=0;i<n;i++)
-			if(arr[i]==0)
-			   cout<<arr[i]<<" ";
-		for(int i=0;i<n;i++)
-			if(arr[i]==1)
-			   cout<<arr[i]<<" ";
-		for(int i=0;i<n;i++)
-			if(arr[i]==2)
-			   cout<<arr[i]<<" ";
-		   /*for(int j=0;j<n-i-1;j++){
-		   	  if(arr[j]>arr[j+1]){
-		   	  	 int temp=arr[j];
-		   	  	 arr[j]=arr[j+1];
-		   	  	 arr[j+1]=temp;
-		   	  }
-		   }
-		for(int i=0;i<n;i++)
-		   cout<<arr[i]<<" ";*/
-		cout<<"\n";
+		for( int i = 0;i < n; i++) {
+			if( arr[i] == 0 ) {
+			   cout << arr[i] << " ";
+			}
+		}
+		for(int i = 0;i < n; i++) {
+			if( arr[i] == 1 ) {
+			   cout << arr[i] << " ";
+			}
+		}
+		for(int i = 0;i < n; i++) {
+			if( arr[i] == 2 ) {
+			   cout << arr[i] << " ";
+			}
+		}
+		cout << "\n";
 	}
-return 0;
+	return 0;
 }
+
+/*
+	Follow some instructions 
+	1. use only those header files which you are using in your code.
+	2. Work on indentation
+	3. Variable name should be related.
+	4. your code should look good.
+	
+*/
